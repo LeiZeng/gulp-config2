e2fe82f1e24fdf4e16b9eeb50ccb16570a4bf8a1
Eric Zeng <zengleizzz@gmail.com>
2015-09-03 22:18:01 +1000

    task management

--- src/task.js
+++ src/task.js
@@ -7,8 +7,8 @@ import _ from 'ramda'
 import config from './config'
 
 // callbackOrNot :: taskFunc -> isCB:Bool -> taskFunc
-const callbackOrNot = _.curry((task, isCB) => {
-  return isCB ? (callback) => task(callback) : task
+const callbackOrNot = _.curry((func) => {
+  return func.length ? (callback) => func(callback) : func
 })
 
 // getFuncPath :: -> task -> taskPath
@@ -21,12 +21,26 @@ const isLocalFunction = _.curry((taskFile) => {
   return taskFile.indexOf('./') > -1
 })
 
+// wrapPlaceholder :: Taskfunction -> Taskfunction
+const wrapPlaceholder = _.curry((func) => {
+  return func.length ?
+    (callback) => {
+      return func(callback)
+    } :
+    () => {
+      return func()
+    }
+})
+
 // getTaskFunc :: fileName -> gulpTaskFunc
-export const getTaskFunc = _.ifElse(_.is(Function),
-  _.identity,
-  _.compose(
-    require,
-    _.ifElse(isLocalFunction, getFuncPath, _.identity)
+export const getTaskFunc = _.compose(
+  wrapPlaceholder,
+  _.ifElse(_.is(Function),
+    _.identity,
+    _.compose(
+      require,
+      _.ifElse(isLocalFunction, getFuncPath, _.identity)
+    )
   )
 )
 
